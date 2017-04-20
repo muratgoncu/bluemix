dc826bbcc378696939262c1c351768737724c9fe
muratgoncu <muratgncu@gmail.com>
2017-04-20 15:44:35 +0300

    first commit

---
+++ bluemix-mysql-nodejs/public/javascripts/main.js
@@ -0,0 +1,35 @@
+/*eslint-env jquery */
+$(document).ready(function() {
+
+  function reload() {
+    $('.hidden').fadeOut();
+    $('displayOutput').empty();
+    $.get( '/words', function(data) {
+      var rendered = "<ul>";
+      data.forEach(function(item) {
+        rendered = rendered + "<li title="+item.id+"> " + item.name +"</li>";
+      });
+      rendered = rendered + "</ul>";
+
+      $('#displayOutput').html(rendered);
+    });
+    $('.hidden').fadeIn();
+  }
+
+  $('#add-word').submit(function(e) {
+    e.preventDefault();
+
+    $.ajax({
+      url: '/words',
+      type: 'PUT',
+      data: $(this).serialize(),
+      success: function(data) {
+        reload();
+      }
+    });
+  });
+
+  // load data on start
+  reload();
+
+});
